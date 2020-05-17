a08dfd31fcb9bb7cfa4e1d7772b1fc6b3669733a
SKY <onthsky@gmail.com>
2020-05-17 16:12:12 +0300

    Add project files.

---
+++ Interface3/ICustomerDal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface3
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Delete();
+        void Update();
+    }
+
+    class SqlServerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated");
+        }
+    }
+    class JavaServerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Java Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Java Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Java Updated");
+        }
+    }
+    class CServerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("C# Added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("C# Deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("C# Updated");
+        }
+    }
+}
---
+++ Interface3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+// Seneryo Sirketin verilerini hem sql server hemde oracle veri tabainina yazmak istiyoruz .
+namespace Interface3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerDal(),
+                new JavaServerDal(),
+                new CServerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+                customerDal.Update();
+            }
+        }
+    }
+}
